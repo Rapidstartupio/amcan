ddbc424caeb217119460621067843158a9497c2b
Mohammed Nasiri <mksnasiri@gmail.com>
2023-08-28 00:08:18 +0100

    customuzie oauth2

--- app/Claims/CustomClaim.php
+++ app/Claims/CustomClaim.php
@@ -10,8 +10,8 @@ class CustomClaim
     public function handle(AccessToken $token, $next)
     {
         $user = User::find($token->getUserIdentifier());
-        $token->addClaim('report_id', $user->equifax_report_id);
-        $token->addClaim('openid', 'string');
+        //$token->addClaim('report_id', $user->equifax_report_id);
+        //$token->addClaim('openid', 'string');
         return $next($token);
     }
 }
