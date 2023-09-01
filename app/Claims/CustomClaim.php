b47634df1f3994032a7a961b8aa2442a2da394d1
Mohammed Nasiri <mksnasiri@gmail.com>
2023-09-01 22:30:17 +0100

    update equifax api

--- app/Claims/CustomClaim.php
+++ app/Claims/CustomClaim.php
@@ -11,13 +11,13 @@ class CustomClaim
     public function handle(AccessToken $token, $next)
     {
         $user = User::find($token->getUserIdentifier());
-        if (!$user->equifax_report_id) {
-            $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->post('https://api.uat.equifax.ca/v2/oauth/token', [
-                'grant_type' => 'client_credentials',
-                'scope' => 'https://api.equifax.ca/v1/credithealth'
-            ]);
-            dd($response);
-        }
+        // if (!$user->equifax_report_id) {
+        //     $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->post('https://api.uat.equifax.ca/v2/oauth/token', [
+        //         'grant_type' => 'client_credentials',
+        //         'scope' => 'https://api.equifax.ca/v1/credithealth'
+        //     ]);
+        //     dd($response);
+        // }
         $token->addClaim('report_id', $user->equifax_report_id);
         //$token->addClaim('openid', 'string');
         return $next($token);
