45e8066463a28a2ea4350fc36913c1ca22a60cbb
Mohammed Nasiri <mksnasiri@gmail.com>
2023-09-01 23:27:29 +0100

    update equifax

--- app/Auth/BearerTokenResponse.php
+++ app/Auth/BearerTokenResponse.php
@@ -4,6 +4,8 @@ namespace App\Auth;
 
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Http;
 
 class BearerTokenResponse extends \League\OAuth2\Server\ResponseTypes\BearerTokenResponse
 {
@@ -20,9 +22,35 @@ class BearerTokenResponse extends \League\OAuth2\Server\ResponseTypes\BearerToke
     {
         $uid = $this->accessToken->getUserIdentifier();
         $user = User::find($uid);
+        $id_token = null;
+
+        try {
+            $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->asForm()->post('https://api.uat.equifax.ca/v2/oauth/token', [
+                'grant_type' => 'client_credentials',
+                'scope' => 'https://api.equifax.ca/v1/credithealth'
+            ]);
+            $res = $response->object();
+            if ($res->access_token) {
+                $response = Http::withToken($res->access_token)
+                    ->post('https://api.uat.equifax.ca/v1/credithealth/reportId/retrieve', [
+                        'customerInfo' => ['memberNumber' => '999FZ03391', "securityCode" => "99"],
+                        'personalInfo' => [
+                            'firstName' => 'Patric', "lastName" => "Mcafee", "idpKey" => "1", 'middleName' => '', 'dob' => '1984-10-12',
+                            'address' => ["civicNumber" => "123", "streetName" => "Main street", "suite" => "", "city" => "Montréal", "province" => "QC", "postalCode" => "H2Y2V5"]
+                        ]
+                    ]);
+                $res = $response->object();
+                if (isset($res->data->reportId)) {
+                    $user->equifax_report_id = $res->data->reportId;
+                    $user->save();
+                }
+            }
+            $id_token = JWTAuth::claims(['report_id' => $user->equifax_report_id])->fromUser($user);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
         return [
-            // 'report_id' => 'abc',
-            //'id_token' => $user->equifax_report_id,
+            'id_token' => $id_token
         ];
     }
 }
--- app/Claims/CustomClaim.php
+++ app/Claims/CustomClaim.php
@@ -11,32 +11,32 @@ class CustomClaim
     public function handle(AccessToken $token, $next)
     {
         $user = User::find($token->getUserIdentifier());
-        try {
-            $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->asForm()->post('https://api.uat.equifax.ca/v2/oauth/token', [
-                'grant_type' => 'client_credentials',
-                'scope' => 'https://api.equifax.ca/v1/credithealth'
-            ]);
-            $res = $response->object();
-            if ($res->access_token) {
-                $response = Http::withToken($res->access_token)
-                    ->post('https://api.uat.equifax.ca/v1/credithealth/reportId/retrieve', [
-                        'customerInfo' => ['memberNumber' => '999FZ03391', "securityCode" => "99"],
-                        'personalInfo' => [
-                            'firstName' => 'Patric', "lastName" => "Mcafee", "idpKey" => "1", 'middleName' => '', 'dob' => '1984-10-12',
-                            'address' => ["civicNumber" => "123", "streetName" => "Main street", "suite" => "", "city" => "Montréal", "province" => "QC", "postalCode" => "H2Y2V5"]
-                        ]
-                    ]);
-                $res = $response->object();
-                if (isset($res->data->reportId)) {
-                    $user->equifax_report_id = $res->data->reportId;
-                    $user->save();
-                }
-            }
-            //if (!$user->equifax_report_id) {}
-            $token->addClaim('report_id', $user->equifax_report_id);
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
+        // try {
+        //     $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->asForm()->post('https://api.uat.equifax.ca/v2/oauth/token', [
+        //         'grant_type' => 'client_credentials',
+        //         'scope' => 'https://api.equifax.ca/v1/credithealth'
+        //     ]);
+        //     $res = $response->object();
+        //     if ($res->access_token) {
+        //         $response = Http::withToken($res->access_token)
+        //             ->post('https://api.uat.equifax.ca/v1/credithealth/reportId/retrieve', [
+        //                 'customerInfo' => ['memberNumber' => '999FZ03391', "securityCode" => "99"],
+        //                 'personalInfo' => [
+        //                     'firstName' => 'Patric', "lastName" => "Mcafee", "idpKey" => "1", 'middleName' => '', 'dob' => '1984-10-12',
+        //                     'address' => ["civicNumber" => "123", "streetName" => "Main street", "suite" => "", "city" => "Montréal", "province" => "QC", "postalCode" => "H2Y2V5"]
+        //                 ]
+        //             ]);
+        //         $res = $response->object();
+        //         if (isset($res->data->reportId)) {
+        //             $user->equifax_report_id = $res->data->reportId;
+        //             $user->save();
+        //         }
+        //     }
+        //     //if (!$user->equifax_report_id) {}
+        //     $token->addClaim('report_id', $user->equifax_report_id);
+        // } catch (\Throwable $th) {
+        //     //throw $th;
+        // }
 
         //$token->addClaim('openid', 'string');
         return $next($token);
