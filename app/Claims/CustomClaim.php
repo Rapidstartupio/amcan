d8e113d27e588a1c5004f4a8062de33ff60df1e4
Mohammed Nasiri <mksnasiri@gmail.com>
2023-09-01 23:12:35 +0100

    update equifax

--- app/Claims/CustomClaim.php
+++ app/Claims/CustomClaim.php
@@ -11,7 +11,7 @@ class CustomClaim
     public function handle(AccessToken $token, $next)
     {
         $user = User::find($token->getUserIdentifier());
-        if (!$user->equifax_report_id) {
+        try {
             $response = Http::withBasicAuth(env('EQUIFAX_CLIENT_ID'), env('EQUIFAX_SECRET_ID'))->asForm()->post('https://api.uat.equifax.ca/v2/oauth/token', [
                 'grant_type' => 'client_credentials',
                 'scope' => 'https://api.equifax.ca/v1/credithealth'
@@ -26,10 +26,18 @@ class CustomClaim
                             'address' => ["civicNumber" => "123", "streetName" => "Main street", "suite" => "", "city" => "Montréal", "province" => "QC", "postalCode" => "H2Y2V5"]
                         ]
                     ]);
-                dd($response->object());
+                $res = $response->object();
+                if (isset($res->data->reportId)) {
+                    $user->equifax_report_id = $res->data->reportId;
+                    $user->save();
+                }
             }
+            //if (!$user->equifax_report_id) {}
+            $token->addClaim('report_id', $user->equifax_report_id);
+        } catch (\Throwable $th) {
+            //throw $th;
         }
-        $token->addClaim('report_id', $user->equifax_report_id);
+
         //$token->addClaim('openid', 'string');
         return $next($token);
     }
