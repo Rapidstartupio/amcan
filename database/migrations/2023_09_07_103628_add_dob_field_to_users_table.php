4b38afee07bb4eb9e4cd5fcea887b006399053a0
Umer Saeed <umer2saeed@gmail.com>
2023-09-07 16:09:26 +0500

    DOB field added to users table

--- database/migrations/2023_09_07_103628_add_dob_field_to_users_table.php
+++ database/migrations/2023_09_07_103628_add_dob_field_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('equifax_report_id')->nullable()->after('role_id');
+            $table->date('dob')->nullable()->after('avatar');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('equifax_report_id');
+            $table->dropColumn('dob');
         });
     }
 };
