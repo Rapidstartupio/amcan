3e9010b058e9e6d912eadd58cdd79bff9ef95c84
devnasiri <mksnasiri@gmail.com>
2023-09-14 13:42:45 +0100

    update jwt config

--- config/jwt.php
+++ config/jwt.php
@@ -8,8 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use Illuminate\Support\Facades\Log;
-Log::info(file_get_contents(storage_path('oauth-public.key')));
+
 return [
 
     /*
